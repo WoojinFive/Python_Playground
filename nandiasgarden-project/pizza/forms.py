64cbfcfc0f4c3b88990ecb488ffee41c0cf6de2f
Woojin Oh <kh3996@gmail.com>
2020-12-06 20:37:14 -0400

    working with widgets

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -2,14 +2,21 @@ from django import forms
 from .models import Pizza
 
 
-# class PizzaForm(forms.Form):
-#     topping1 = forms.CharField(label='Topping 1', max_length=100)
-#     topping2 = forms.CharField(label='Topping 2', max_length=100)
-#     size = forms.ChoiceField(label='Size', choices=[(
-#         'Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')])
+class PizzaForm(forms.Form):
+    # topping1 = forms.CharField(
+    #     label='Topping 1', max_length=100, widget=forms.PasswordInput)
+    # topping2 = forms.CharField(label='Topping 2', max_length=100)
 
-class PizzaForm(forms.ModelForm):
-    class Meta:
-        model = Pizza
-        fields = ['topping1', 'topping2', 'size']
-        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+    # toppings = forms.MultipleChoiceField(
+    #     choices=[('pep', 'Pepperoni'), ('cheese', 'Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
+
+    topping1 = forms.CharField(label='Topping 1', max_length=100)
+    topping2 = forms.CharField(label='Topping 2', max_length=100)
+    size = forms.ChoiceField(label='Size', choices=[(
+        'Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')])
+
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ['topping1', 'topping2', 'size']
+#         labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
