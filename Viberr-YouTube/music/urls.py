4bdc4e51724ab9fc2f6f380c9c89f3e7c80b9ca8
Woojin Oh <kh3996@gmail.com>
2020-12-08 14:32:49 -0400

    Generic Views

--- Viberr-YouTube/music/urls.py
+++ Viberr-YouTube/music/urls.py
@@ -4,10 +4,12 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<pk>/', views.DetailView.as_view(), name='detail'),
     # /music/
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     # /music/<id>/
-    path('<int:id>/', views.detail, name='detail'),
+    # path('<int:id>/', views.detail, name='detail'),
     # /music/<id>/favorite/
-    path('<int:id>/favorite/', views.favorite, name="favorite"),
+    # path('<int:id>/favorite/', views.favorite, name="favorite"),
 ]
--- Viberr-YouTube/music/views.py
+++ Viberr-YouTube/music/views.py
@@ -1,40 +1,57 @@
+from django.views import generic
+from .models import Album
+
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+
 # from django.template import loader
 # from django.http import HttpResponse
 # from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+# from django.shortcuts import render, get_object_or_404
+# from .models import Album, Song
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    # template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'music/index.html', context)
+# def index(request):
+#     all_albums = Album.objects.all()
+#     # template = loader.get_template('music/index.html')
+#     context = {
+#         'all_albums': all_albums,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'music/index.html', context)
 
 
-def detail(request, id):
-    # try:
-    #     album = Album.objects.get(id=id)
-    # except Album.DoesNotExist:
-    #     raise Http404("Album does not exist")
-    album = get_object_or_404(Album, id=id)
-    return render(request, 'music/detail.html', {'album': album})
+# def detail(request, id):
+#     # try:
+#     #     album = Album.objects.get(id=id)
+#     # except Album.DoesNotExist:
+#     #     raise Http404("Album does not exist")
+#     album = get_object_or_404(Album, id=id)
+#     return render(request, 'music/detail.html', {'album': album})
 
 
-def favorite(request, id):
-    album = get_object_or_404(Album, id=id)
-    try:
-        selected_song = album.song_set.get(id=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': 'You did not select a valid song',
-        })
-    else:
-        # selected_song.is_favorite = not selected_song.is_favorite
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+# def favorite(request, id):
+#     album = get_object_or_404(Album, id=id)
+#     try:
+#         selected_song = album.song_set.get(id=request.POST['song'])
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request, 'music/detail.html', {
+#             'album': album,
+#             'error_message': 'You did not select a valid song',
+#         })
+#     else:
+#         # selected_song.is_favorite = not selected_song.is_favorite
+#         selected_song.is_favorite = True
+#         selected_song.save()
+#         return render(request, 'music/detail.html', {'album': album})
