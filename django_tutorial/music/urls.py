2c796d803ff445b9a100157a3a602feb26eac2cd
Woojin Oh <kh3996@gmail.com>
2020-12-08 10:49:14 -0400

    writing another view

--- django_tutorial/music/urls.py
+++ django_tutorial/music/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
+    #  /music/71/
+    path('<int:id>/', views.detail, name='detail'),
 ]
--- django_tutorial/music/views.py
+++ django_tutorial/music/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>This is the music app homepage</h1>')
+
+
+def detail(request, id):
+    return HttpResponse('<h2>Details for Album id: ' + str(id) + '</h2>')
