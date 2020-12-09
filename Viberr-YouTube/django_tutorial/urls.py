ea9a0f18c79f875df582d69639723b4c738eda75
Woojin Oh <kh3996@gmail.com>
2020-12-09 10:23:58 -0400

    REST API Serializer JSON

--- Viberr-YouTube/django_tutorial/urls.py
+++ Viberr-YouTube/django_tutorial/urls.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
+from companies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/', include('music.urls'))
+    path('music/', include('music.urls')),
+    path('stocks/', views.StockList.as_view()),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = format_suffix_patterns(urlpatterns)
