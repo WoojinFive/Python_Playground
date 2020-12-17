c50002b4c2f14317c0a478f72fdf8500434ef078
woojinfive <kh3996@gmail.com>
2020-12-17 13:27:02 -0400

    form

--- myfirstproject/myfirstapp/views.py
+++ myfirstproject/myfirstapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .forms import *
 
 
 # Create your views here.
@@ -25,3 +26,64 @@ def intro(request, name, age):
 
 def myfirstpage(request):
     return render(request, 'index.html')
+
+
+def mysecondpage(request):
+    return render(request, 'second.html')
+
+
+def mythirdpage(request):
+    var = "hello world"
+    greeting = "hey how are you"
+    fruits = ["apple", "mango", "banana"]
+    num1, num2 = 3, 5
+    ans = num1 > num2
+    mydictionary = {
+        "var": var,
+        "msg": greeting,
+        "myfruits": fruits,
+        "num1": num1,
+        "num2": num2,
+        "ans": ans
+    }
+    return render(request, 'third.html', context=mydictionary)
+
+
+def myimagepage(request):
+    return render(request, 'imagepage.html')
+
+
+def myimagepage2(request):
+    return render(request, 'imagepage2.html')
+
+
+def myimagepage3(request):
+    return render(request, 'imagepage3.html')
+
+
+def myimagepage4(request):
+    return render(request, 'imagepage4.html')
+
+
+def myform(request):
+    return render(request, 'myform.html')
+
+
+def submitmyform(request):
+    mydictionary = {
+        "val1": request.POST['mytext'],
+        "val2": request.POST['mytextarea'],
+        "method": request.method
+    }
+    return JsonResponse(mydictionary)
+
+
+def myform2(request):
+    if request.method == "POST":
+        pass
+    elif request.method == "GET":
+        form = FeedbackForm()
+        mydictionary = {
+            "form": form
+        }
+        return render(request, 'myform2.html', context=mydictionary)
