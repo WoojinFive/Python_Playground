019adab3d786d1166da30a0b8f63cb66276fcf89
woojinfive <kh3996@gmail.com>
2021-01-22 11:38:15 -0400

    Caesar Cipher Part 2 - Decryption

--- 100_days_of_code/Day8/main.py
+++ 100_days_of_code/Day8/main.py
@@ -3,19 +3,8 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 def encrypt(text, shift):
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
     new_text = ""
     for letter in text:
         index = alphabet.index(letter)
@@ -25,5 +14,25 @@ def encrypt(text, shift):
         
     print(f"The encoded text is {new_text}")
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+    #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+    #e.g. 
+    #cipher_text = "mjqqt"
+    #shift = 5
+    #plain_text = "hello"
+    #print output: "The decoded text is hello"
+    new_text = ""
+    for letter in text:
+        index = alphabet.index(letter)
+        new_index = (index - shift) % 26
+        new_letter = alphabet[new_index]
+        new_text += new_letter
+    
+    print(f"The decoded text is {new_text}")
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
