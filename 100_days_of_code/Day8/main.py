8ec8f4e55c40155692dbd7b02625e649b746bc92
woojinfive <kh3996@gmail.com>
2021-01-22 10:49:34 -0400

    Paint Area Calculator

--- 100_days_of_code/Day8/main.py
+++ 100_days_of_code/Day8/main.py
@@ -1,14 +1,22 @@
-def greet(name):
-    print(f"Hello, {name}")
-    print(f"How do yo do {name}?")
-    print("Isn't the weather nice today?")
+#Write your code below this line 👇
+import math
 
-greet("Angela")
+def paint_calc(height, width, cover):
+    number_of_cans = math.ceil((height * width) / cover)
+    print(f"You'll need {number_of_cans} cans of paint.")
 
-#Functions with more than 1 input
-def greet2(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}")
 
-# greet2("Angela", "London")
-greet2(location="Seoul", name="Angela")
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
