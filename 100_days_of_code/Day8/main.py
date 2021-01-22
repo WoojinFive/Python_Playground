3138a56731bb96c0456cc99bd34362ea96a7282b
woojinfive <kh3996@gmail.com>
2021-01-22 10:39:59 -0400

    Positional vs. Keyword Arguments

--- 100_days_of_code/Day8/main.py
+++ 100_days_of_code/Day8/main.py
@@ -4,3 +4,11 @@ def greet(name):
     print("Isn't the weather nice today?")
 
 greet("Angela")
+
+#Functions with more than 1 input
+def greet2(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+# greet2("Angela", "London")
+greet2(location="Seoul", name="Angela")
