2649d50594f43b90fba60946d2bc959f482bf7c4
woojinfive <kh3996@gmail.com>
2021-01-22 11:30:59 -0400

    Caesar Cipher Part 1 - Encryption

--- 100_days_of_code/Day8/main.py
+++ 100_days_of_code/Day8/main.py
@@ -1,21 +1,29 @@
-#Write your code below this line 👇
-def prime_checker(number):
-    is_prime = True
-    for num in range(2, number):
-        if number % num == 0:
-            is_prime = False
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    if is_prime:
-        print("It's a prime number.")
-    else:
-        print("It's not a prime number.")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
 
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-#Write your code above this line 👆
-    
-#Do NOT change any of the code below👇
-n = int(input("Check this number: "))
-prime_checker(number=n)
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+    new_text = ""
+    for letter in text:
+        index = alphabet.index(letter)
+        new_index = (index + shift) % 26
+        new_letter = alphabet[new_index]
+        new_text += new_letter
+        
+    print(f"The encoded text is {new_text}")
 
-
-
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(text, shift)
