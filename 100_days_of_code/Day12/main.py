ed570c468c7a6cc1ef368f66eba66119a31531fc
woojinfive <kh3996@gmail.com>
2021-01-29 10:19:39 -0400

    How to Modify a Global Variable

--- 100_days_of_code/Day12/main.py
+++ 100_days_of_code/Day12/main.py
@@ -3,14 +3,9 @@
 enemies = 1
 
 def increase_enemies():
+    global enemies
     enemies = 2
     print(f"enemies inside function: {enemies}")
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
-
-# Local Scope
-
-def drint_potion():
-    potion_strength = 2
-    print(potion_strength)
