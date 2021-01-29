7b999b3adfb00a97fd583134d0bf7ca51d0f1357
woojinfive <kh3996@gmail.com>
2021-01-29 12:41:45 -0400

    The Number Guessing Game my solution

--- 100_days_of_code/Day12/main.py
+++ 100_days_of_code/Day12/main.py
@@ -1,61 +1,43 @@
-################### Scope ####################
+#Number Guessing Game Objectives:
 
-enemies = 1
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
-def increase_enemies():
-    enemies = 2
-    print(f"enemies inside function: {enemies}")
+from art import logo
+from random import randint
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Typa 'easy' or 'hard': ").lower()
 
-# Local Scope
+should_continue = True
+target = randint(1, 100)
+attemps = 10 if difficulty == 'easy' else 5
 
-def drink_potion():
-    potion_strength = 2
-    print(potion_strength)
-
-drink_potion()
-print(potion_strength)
-
-# Global Scope
-player_health = 10
-
-def game():
-    def drink_potion():
-        potion_strength = 2
-        print(player_health)
-
-    drink_potion()
-
-print(player_health)
-
-# There is no Block Scope
-
-game_level = 3
-
-def create_enemy():
-    enemies = ["Skeleton", "Zombie", "Alien"]
-    if game_level < 5:
-        new_enemy = enemies[0]
-
-    print(new_enemy)
+def compare(num):
+    if num > target:
+        print("Too high.")
+        return True
+    elif num < target:
+        print("Too low.")
+        return True
+    else:
+        print(f"You got it! The answer was {target}.")
+        return False
 
 
-# Modifying Global Scope
-
-enemies = 1
-
-def increase_enemies():
-    print(f"enemies inside function: {enemies}")
-    return enemies + 1
-
-enemies = increase_enemies()
-print(f"enemies outside function: {enemies}")
-
-#Global Constants
-
-PI = 3.14159
-URL = "https://www.google.com"
-TWITTER_HANDLE = "@yu_angela"
-
+while attemps > 0 and should_continue:
+    print(f"You have {attemps} attemps remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    should_continue = compare(guess)
+    attemps -= 1
+    if attemps == 0 and should_continue:
+        print("You've run out of guesses, you lose")
+    else:
+        print("Guess again.")
