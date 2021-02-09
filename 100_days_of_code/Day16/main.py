3f01032c6cb8bf4a492d40d6e0e3e38ba925fda9
woojinfive <kh3996@gmail.com>
2021-02-09 16:02:19 -0400

    Building the Coffee Machine in OOP

--- 100_days_of_code/Day16/main.py
+++ 100_days_of_code/Day16/main.py
@@ -1,19 +1,3 @@
-# from turtle import Turtle, Screen
-
-# timmy = Turtle()
-# my_screen = Screen()
-
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
-
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-
-print(table)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
