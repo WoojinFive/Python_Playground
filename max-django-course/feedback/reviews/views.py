0300885907595db6408f578cb025c2359700308c
woojinfive <kh3996@gmail.com>
2021-06-04 13:45:28 -0300

    Manual Form Validation & the Problems with "that"

--- max-django-course/feedback/reviews/views.py
+++ max-django-course/feedback/reviews/views.py
@@ -6,11 +6,17 @@ from django.shortcuts import redirect, render
 def review(request):
     if request.method == 'POST':
         entered_username = request.POST['username']
-        print(entered_username)
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
         return HttpResponseRedirect("/thank-you")
 
     return render(request, "reviews/review.html")
 
 
 def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html", {
+        "has_error": False
+    })
