f765a6874a575e3390566c345a4ffd2de5bb2d65
woojinfive <kh3996@gmail.com>
2021-06-04 21:20:56 -0300

    Saving Data with a Modelform

--- max-django-course/feedback/reviews/views.py
+++ max-django-course/feedback/reviews/views.py
@@ -9,14 +9,16 @@ from .models import Review
 
 def review(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        existing_data = Review.objects.get(pk=1) # for updating exist data
+        form = ReviewForm(request.POST, instance=existing_data) # for updating exist data
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating'])
-            review.save()
+            # review = Review(
+            #     user_name=form.cleaned_data['user_name'],
+            #     review_text=form.cleaned_data['review_text'],
+            #     rating=form.cleaned_data['rating'])
+            # review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
 
     else:
