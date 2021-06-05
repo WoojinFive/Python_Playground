a1670ad23f8e243bc680f80e0c7f5afda7aa0c95
woojinfive <kh3996@gmail.com>
2021-06-04 23:03:35 -0300

    TemplateView

--- max-django-course/feedback/reviews/views.py
+++ max-django-course/feedback/reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -51,5 +52,13 @@ class ReviewView(View):
 #     })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
