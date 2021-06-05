434b5b355938a615bf0abeae4339a5fc3032c74a
woojinfive <kh3996@gmail.com>
2021-06-04 23:19:28 -0300

    The ListView

--- max-django-course/feedback/reviews/views.py
+++ max-django-course/feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -64,14 +65,25 @@ class ThankYouView(TemplateView):
 #     return render(request, "reviews/thank_you.html")
 
 
-class ReviewsListView(TemplateView):
-    template_name = "reviews/review_list.html"
+# class ReviewsListView(TemplateView):
+#     template_name = "reviews/review_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
 
 
 class SingleReviewView(TemplateView):
