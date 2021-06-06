68c6fd26ffc234f5fc27641e17b9d23ed4aa1b41
woojinfive <kh3996@gmail.com>
2021-06-05 21:58:38 -0300

    Using a CreateView

--- max-django-course/feedback/profiles/views.py
+++ max-django-course/feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -14,22 +15,29 @@ from .models import UserProfile
 #             dest.write(chunk)
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            # store_file(request.FILES['image'])
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES['image'])
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
