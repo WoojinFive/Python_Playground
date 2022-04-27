72f85a48a0670758637d0246347abcf1ce5753e5
woojinfive <kh3996@gmail.com>
2022-04-26 22:07:21 -0300

    Drawing Different Shapes final

--- 100_days_of_code/Day18/main.py
+++ 100_days_of_code/Day18/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, shape
 from random import randint
 
 screen = Screen()
@@ -7,16 +7,16 @@ screen.colormode(255)
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 
-
-for num_sides in range(4,9):
-    length = 100
-    angle = 360/num_sides
-
-    timmy_the_turtle.color((randint(0, 255), randint(0, 255), randint(0, 255)))
-
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    
     for _ in range(num_sides):
-        timmy_the_turtle.forward(length)
+        timmy_the_turtle.forward(100)
         timmy_the_turtle.right(angle)
 
+
+for shape_side_n in range(3, 11):
+    timmy_the_turtle.color((randint(0, 255), randint(0, 255), randint(0, 255)))
+    draw_shape(shape_side_n)
 
 screen.exitonclick()
