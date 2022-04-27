f654ad984738169510f8dae86e7d4b834ef4f2f0
woojinfive <kh3996@gmail.com>
2022-04-26 21:53:56 -0300

    Turtle Challenge 2 - Draw a Dashed Line

--- 100_days_of_code/Day18/main.py
+++ 100_days_of_code/Day18/main.py
@@ -5,7 +5,12 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 
 for _ in range(4):
-    timmy_the_turtle.forward(100)
+    for _ in range(10):
+        timmy_the_turtle.forward(5)
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(5)
+        timmy_the_turtle.pendown()
+
     timmy_the_turtle.right(90)
 
 screen = Screen()
