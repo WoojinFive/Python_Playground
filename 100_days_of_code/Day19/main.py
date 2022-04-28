b5bb0dc5ffd71e8632c8b06a2d35166e0e47a367
woojinfive <kh3996@gmail.com>
2022-04-27 22:21:33 -0300

    Understanding the Turtle Coordinate System

--- 100_days_of_code/Day19/main.py
+++ 100_days_of_code/Day19/main.py
@@ -1,30 +1,15 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
-screen.listen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.backward(10)
-
-def turn_left():
-    current_heading = tim.heading()
-    tim.setheading(current_heading + 10)
-
-def turn_right():
-    current_heading = tim.heading()
-    tim.setheading(current_heading - 10)
-
-def clear_drawing():
-    tim.reset()
-
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="c", fun=clear_drawing)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 screen.exitonclick()
