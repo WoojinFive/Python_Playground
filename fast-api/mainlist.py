a64016fe90b15f42cad387cc9825403ef0f52028
woojinfive <kh3996@gmail.com>
2023-10-27 22:22:10 -0300

    completed cities API

--- fast-api/mainlist.py
+++ fast-api/mainlist.py
@@ -17,6 +17,12 @@ class City(BaseModel):
     timezone: str
 
 
+class CityModify(BaseModel):
+    id: int
+    name: str
+    timezone: str
+
+
 templates = Jinja2Templates(directory="templates")
 
 
@@ -24,26 +30,39 @@ templates = Jinja2Templates(directory="templates")
 def root():
     return {"Hello": "World!"}
 
-@app.get("/cities")
-def get_cities():
-    results = []
+@app.get("/cities", response_class=HTMLResponse)
+def get_cities(request: Request):
+    context = {}
+
+    rsCity = []
+
+    cnt = 0
     for city in db:
-        strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
-        r = requests.get(strs)
-        print("@@@@@@@@@@@", r.json())
+        str = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
+
+        r = requests.get(str)
         cur_time = r.json()['datetime']
-        results.append({'name': city['name'], 'timezone': city['timezone'], 'current_time': cur_time})
 
-    return results
+        cnt += 1
 
-@app.get("/cities/{city_id}")
-def get_city(city_id: int):
+        rsCity.append({'id': cnt, 'name':city['name'], 'timezone':city['timezone'], 'current_time': cur_time})
+
+    context['request'] = request
+    context['rsCity'] = rsCity
+
+    return templates.TemplateResponse("city_list.html", context)
+
+
+@app.get('/cities/{city_id}', response_class=HTMLResponse)
+def get_city(request: Request, city_id: int):
     city = db[city_id - 1]
-    strs = f"http://worldtimeapi.org/api/timezone/{city['timezone']}"
-    r = requests.get(strs)
+    r = requests.get(f"http://worldtimeapi.org/api/timezone/{city['timezone']}")
     cur_time = r.json()['datetime']
-    
-    return {'name': city['name'], 'timezone': city['timezone'], 'current_time': cur_time}
+
+    # return {'name':city['name'], 'timezone':city['timezone'], 'current_time': cur_time}
+    context = {'request':request, 'name':city['name'], 'timezone':city['timezone'], 'current_time': cur_time}
+    return templates.TemplateResponse("city_detail.html", context)
+
 
 @app.post("/cities")
 def create_city(city: City):
@@ -51,8 +70,18 @@ def create_city(city: City):
 
     return db[-1]
 
-@app.delete("/cities/{city_id}")
+
+@app.put('/cities')
+def modify_city(city: CityModify):
+
+    db[city.id-1] = { 'name': city.name, 'timezone': city.timezone }
+
+    return db[city.id-1]
+
+
+
+@app.delete('/cities/{city_id}')
 def delete_city(city_id: int):
     db.pop(city_id - 1)
 
-    return {}
+    return {'result_msg':'Deleted...'}
