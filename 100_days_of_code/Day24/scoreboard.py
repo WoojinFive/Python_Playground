aafc1f4cf48ae8af197367fb5cba1bc073484586
woojinfive <kh3996@gmail.com>
2025-04-09 10:25:33 -0300

    read and write the high score to a file

--- 100_days_of_code/Day24/scoreboard.py
+++ 100_days_of_code/Day24/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_score_from_file()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,9 +22,19 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_score_to_file(self.high_score)
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def read_score_from_file(self):
+        with open("data.txt", mode="r") as data:
+            score = int(data.read())
+            return score
+        
+    def update_score_to_file(self, score):
+        with open("data.txt", mode="w") as data:
+            data.write("f{score}")
